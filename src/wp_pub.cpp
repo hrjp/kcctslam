eb817dcf03067e841e9209c9e62afb69788e8e5d
Toshihiko Shimizu <kcct-ts8@g.kobe-kosen.ac.jp>
2019-09-09 17:51:10 +0900

    waypointで安定して動いた

--- src/wp_pub.cpp
+++ src/wp_pub.cpp
@@ -15,30 +15,33 @@
 #include"csvread.h"
 #include"wpdata.h"
 #include"tf_lis.h"
+#include"wpmarker.h"
 
 using namespace std;
 
  
 int main(int argc, char **argv){
+    int now_wp=0;
     ros::init(argc, argv, "wp_pub");
- 
-   
-    //csvread csv("~/catkin_ws/src/kcctslam/config/waypointdata/wpdata.csv");
     csvread csv("/home/ryo/catkin_ws/src/kcctslam/config/waypointdata/wpdata.csv");
     csv.print();
+    cout<<endl<<csv.wp.size()<<endl;
     tf_lis base;
-    
+    wpmarker wpmarker;
     
     ros::NodeHandle n;
-    int now_wp=0;
+    
+    Goal init_goal_ob(csv.wp.x(now_wp), csv.wp.y(now_wp),csv.wp.qz(now_wp), csv.wp.qw(now_wp));
     while (n.ok())  {
         base.update();
-        if((base.pos-csv.wp.vec[now_wp]).size()<1.0){
+        wpmarker.update(csv.wp,now_wp);
+        if((base.pos-csv.wp.vec[now_wp]).size()<1.0 && now_wp+1<csv.wp.size()){
             now_wp++;
+            Goal goal_ob(csv.wp.x(now_wp), csv.wp.y(now_wp),csv.wp.qz(now_wp), csv.wp.qw(now_wp));
         }
-         Goal goal_ob(csv.wp.x(now_wp), csv.wp.y(now_wp),csv.wp.qz(now_wp), csv.wp.qw(now_wp));
-        cout<<"base_link x="<<base.pos.x<<" y="<<base.pos.y<<" yaw="<<base.pos.yaw;
-        cout<<"waypoint x="<<csv.wp.vec[now_wp].x<<" y="<<csv.wp.vec[now_wp].y<<" yaw="<<csv.wp.vec[now_wp].yaw<<endl;
+        
+        //cout<<"base_link x="<<base.pos.x<<" y="<<base.pos.y<<" yaw="<<base.pos.yaw;
+        //cout<<"waypoint x="<<csv.wp.vec[now_wp].x<<" y="<<csv.wp.vec[now_wp].y<<" yaw="<<csv.wp.vec[now_wp].yaw<<endl;
     }
     
     return 0;
